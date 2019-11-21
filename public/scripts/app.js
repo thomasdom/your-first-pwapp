832c280b29fd52f3ac3eab45f97988d5b7638161
Thomas <tdomingues.ext@wedigital.garden>
2019-11-21 22:46:01 +0100

    Finished Google's Codelabs Tutorial

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -40,7 +40,7 @@ function addLocation() {
   const selected = select.options[select.selectedIndex];
   const geo = selected.value;
   const label = selected.textContent;
-  const location = {label: label, geo: geo};
+  const location = { label: label, geo: geo };
   // Create a new card & get the weather data from the server
   const card = getForecastCard(location);
   getForecastFromNetwork(geo).then((forecast) => {
@@ -91,29 +91,29 @@ function renderForecast(card, data) {
   // Render the forecast data into the card.
   card.querySelector('.description').textContent = data.currently.summary;
   const forecastFrom = luxon.DateTime
-      .fromSeconds(data.currently.time)
-      .setZone(data.timezone)
-      .toFormat('DDDD t');
+    .fromSeconds(data.currently.time)
+    .setZone(data.timezone)
+    .toFormat('DDDD t');
   card.querySelector('.date').textContent = forecastFrom;
   card.querySelector('.current .icon')
-      .className = `icon ${data.currently.icon}`;
+    .className = `icon ${data.currently.icon}`;
   card.querySelector('.current .temperature .value')
-      .textContent = Math.round(data.currently.temperature);
+    .textContent = Math.round(data.currently.temperature);
   card.querySelector('.current .humidity .value')
-      .textContent = Math.round(data.currently.humidity * 100);
+    .textContent = Math.round(data.currently.humidity * 100);
   card.querySelector('.current .wind .value')
-      .textContent = Math.round(data.currently.windSpeed);
+    .textContent = Math.round(data.currently.windSpeed);
   card.querySelector('.current .wind .direction')
-      .textContent = Math.round(data.currently.windBearing);
+    .textContent = Math.round(data.currently.windBearing);
   const sunrise = luxon.DateTime
-      .fromSeconds(data.daily.data[0].sunriseTime)
-      .setZone(data.timezone)
-      .toFormat('t');
+    .fromSeconds(data.daily.data[0].sunriseTime)
+    .setZone(data.timezone)
+    .toFormat('t');
   card.querySelector('.current .sunrise .value').textContent = sunrise;
   const sunset = luxon.DateTime
-      .fromSeconds(data.daily.data[0].sunsetTime)
-      .setZone(data.timezone)
-      .toFormat('t');
+    .fromSeconds(data.daily.data[0].sunsetTime)
+    .setZone(data.timezone)
+    .toFormat('t');
   card.querySelector('.current .sunset .value').textContent = sunset;
 
   // Render the next 7 days.
@@ -121,15 +121,15 @@ function renderForecast(card, data) {
   futureTiles.forEach((tile, index) => {
     const forecast = data.daily.data[index + 1];
     const forecastFor = luxon.DateTime
-        .fromSeconds(forecast.time)
-        .setZone(data.timezone)
-        .toFormat('ccc');
+      .fromSeconds(forecast.time)
+      .setZone(data.timezone)
+      .toFormat('ccc');
     tile.querySelector('.date').textContent = forecastFor;
     tile.querySelector('.icon').className = `icon ${forecast.icon}`;
     tile.querySelector('.temp-high .value')
-        .textContent = Math.round(forecast.temperatureHigh);
+      .textContent = Math.round(forecast.temperatureHigh);
     tile.querySelector('.temp-low .value')
-        .textContent = Math.round(forecast.temperatureLow);
+      .textContent = Math.round(forecast.temperatureLow);
   });
 
   // If the loading spinner is still visible, remove it.
@@ -147,12 +147,12 @@ function renderForecast(card, data) {
  */
 function getForecastFromNetwork(coords) {
   return fetch(`/forecast/${coords}`)
-      .then((response) => {
-        return response.json();
-      })
-      .catch(() => {
-        return null;
-      });
+    .then((response) => {
+      return response.json();
+    })
+    .catch(() => {
+      return null;
+    });
 }
 
 /**
@@ -163,7 +163,21 @@ function getForecastFromNetwork(coords) {
  */
 function getForecastFromCache(coords) {
   // CODELAB: Add code to get weather forecast from the caches object.
-
+  if (!('caches' in window)) {
+    return null;
+  }
+  const url = `${window.location.origin}/forecast/${coords}`;
+  return caches.match(url)
+    .then(response => {
+      if (response) {
+        return response.json();
+      }
+      return null;
+    })
+    .catch(err => {
+      console.error('Error getting data from cache', err);
+      return null;
+    });
 }
 
 /**
@@ -183,7 +197,7 @@ function getForecastCard(location) {
   newCard.querySelector('.location').textContent = location.label;
   newCard.setAttribute('id', id);
   newCard.querySelector('.remove-city')
-      .addEventListener('click', removeLocation);
+    .addEventListener('click', removeLocation);
   document.querySelector('main').appendChild(newCard);
   newCard.removeAttribute('hidden');
   return newCard;
@@ -198,12 +212,16 @@ function updateData() {
     const location = weatherApp.selectedLocations[key];
     const card = getForecastCard(location);
     // CODELAB: Add code to call getForecastFromCache
+    getForecastFromCache(location.geo)
+      .then(forecast => {
+        renderForecast(card, forecast);
+      });
 
     // Get the forecast data from the network.
     getForecastFromNetwork(location.geo)
-        .then((forecast) => {
-          renderForecast(card, forecast);
-        });
+      .then((forecast) => {
+        renderForecast(card, forecast);
+      });
   });
 }
 
@@ -234,7 +252,7 @@ function loadLocationList() {
   if (!locations || Object.keys(locations).length === 0) {
     const key = '40.7720232,-73.9732319';
     locations = {};
-    locations[key] = {label: 'New York City', geo: '40.7720232,-73.9732319'};
+    locations[key] = { label: 'New York City', geo: '40.7720232,-73.9732319' };
   }
   return locations;
 }
@@ -252,9 +270,9 @@ function init() {
   document.getElementById('butRefresh').addEventListener('click', updateData);
   document.getElementById('butAdd').addEventListener('click', toggleAddDialog);
   document.getElementById('butDialogCancel')
-      .addEventListener('click', toggleAddDialog);
+    .addEventListener('click', toggleAddDialog);
   document.getElementById('butDialogAdd')
-      .addEventListener('click', addLocation);
+    .addEventListener('click', addLocation);
 }
 
 init();
